ddb6dbc60ff0e6e16d41e16985b4bbb1f308b365
Erik Krogh Kristensen <erik@webbies.dk>
2016-04-30 23:45:27 +0200

    version bump

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var program = require('commander');
 var doctrine = require("doctrine");
 
 program
-    .version('1.0.2')
+    .version('1.0.3')
     .option('--unwrap', 'Set to delete the leading /**, any * that begins a line, and the trailing */ from the source text.')
     .option('--sloppy', 'allow optional parameters to be specified in brackets')
     .parse(process.argv);
