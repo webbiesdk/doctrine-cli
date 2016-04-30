f07ea610476161e981cdaf73c0f0de4a36bc7695
Erik Krogh Kristensen <erik@webbies.dk>
2016-04-30 22:56:20 +0200

    stable

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var program = require('commander');
 var doctrine = require("doctrine");
 
 program
-    .version('1.0.1')
-    .option('---unwrap ', 'Set to delete the leading /**, any * that begins a line, and the trailing */ from the source text.')
-    .option('--sloppy ', 'allow optional parameters to be specified in brackets')
+    .version('1.0.2')
+    .option('--unwrap', 'Set to delete the leading /**, any * that begins a line, and the trailing */ from the source text.')
+    .option('--sloppy', 'allow optional parameters to be specified in brackets')
     .parse(process.argv);
 
 var content = '';
@@ -12,7 +12,7 @@ process.stdin.resume();
 process.stdin.on('data', function(buf) { content += buf.toString(); });
 process.stdin.on('end', function() {
     var ast = doctrine.parse(
-        content, { unwrap: program.unwrap , sloppy : program.sloppy });
+        content, { unwrap: program.unwrap, sloppy: program.sloppy});
 
     console.log(JSON.stringify(ast));
 });
