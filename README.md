7da6b671a180bbf17e4bc6138dd61f278ef3f0fe
Erik Krogh Kristensen <erik@webbies.dk>
2016-05-20 15:25:04 +0200

    Added ability to keep the process running

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A simple wrapper around doctrine, so one can use it in a command-line setting.
 The JSDoc input is passed to stdin. 
 
 It takes two options, `--unwrap` and `--sloppy`, both match the corrosponding options in doctrine. 
+
+The `--multiple` option allows one to keep the process alive when parsing multiple JSDoc's.
+Just separate each request with the 0 byte, and the results will be printed on a line, ending with a `\n`.
--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-var program = require('commander');
-var doctrine = require("doctrine");
+(function (process, require) {
+    var program = require('commander');
+    var doctrine = require("doctrine");
 
-program
-    .version('1.0.3')
-    .option('--unwrap', 'Set to delete the leading /**, any * that begins a line, and the trailing */ from the source text.')
-    .option('--sloppy', 'allow optional parameters to be specified in brackets')
-    .parse(process.argv);
+    program
+        .version('1.0.4')
+        .option('--unwrap', 'Set to delete the leading /**, any * that begins a line, and the trailing */ from the source text.')
+        .option('--sloppy', 'allow optional parameters to be specified in brackets')
+        .option('--multiple', 'keeps the program alive between requests to parse JSDoc. Each request to the program is separated by the 0 byte. And each result is printed on a line ending with a \\n. ')
+        .parse(process.argv);
 
-var content = '';
-process.stdin.resume();
-process.stdin.on('data', function(buf) { content += buf.toString(); });
-process.stdin.on('end', function() {
-    var ast = doctrine.parse(
-        content, { unwrap: program.unwrap, sloppy: program.sloppy});
+    function parse(content) {
+        return doctrine.parse(content, {unwrap: program.unwrap, sloppy: program.sloppy});
+    }
 
-    console.log(JSON.stringify(ast));
-});
+    var content = '';
+
+    if (program.multiple) {
+        var terminationChar = 0;
+
+        process.stdin.resume();
+        process.stdin.on('data', function(buf) {
+            for (var i = 0; i < buf.length; i++) {
+                var char = buf[i];
+                if (char != terminationChar) {
+                    content += String.fromCharCode(char);
+                } else {
+                    var ast = parse(content);
+
+                    console.log(JSON.stringify(ast));
+
+                    content = '';
+                }
+            }
+        });
+        process.stdin.on('end', function() {
+            console.error("Garbage output, this is only supposed to happen if the process is killed. ")
+        });
+    } else {
+
+        process.stdin.resume();
+        process.stdin.on('data', function(buf) { content += buf.toString(); });
+        process.stdin.on('end', function() {
+            var ast = parse(content);
+
+            console.log(JSON.stringify(ast));
+        });
+    }
+})(process, require);
